0d92a64363b71ab33214029bd839f3c2ea3553b9
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-19 12:19:51 -0400

    Make background colors appear only when mouse is down, and make them stay

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let isMouseDown = false;
+document.addEventListener("mousedown", () => isMouseDown = true);
+document.addEventListener("mouseup", () => isMouseDown = false);
+
 const container = document.querySelector(".container");
 for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
@@ -5,8 +9,13 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const cell = document.createElement("div");
         cell.classList.toggle("cell");
-        cell.addEventListener("mouseenter", (e) => cell.classList.toggle("hovered"));
-        cell.addEventListener("mouseleave", (e) => cell.classList.toggle("hovered"));
+        cell.addEventListener("mouseenter", (e) => {
+            if (isMouseDown) {
+                cell.classList.add("hovered");
+            }
+        });
+        cell.addEventListener("mousedown", () => cell.classList.add("hovered"));
+        // cell.addEventListener("mouseleave", (e) => cell.classList.toggle("hovered"));
         row.append(cell);
     }
     container.append(row);
