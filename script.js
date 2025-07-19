683685edfd63af988d9beb224e2a5f8ee8c8807f
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-19 12:07:55 -0400

    Changed cell background color when hovered

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ for (let i = 0; i < 16; i++) {
     row.classList.toggle("row");
     for (let j = 0; j < 16; j++) {
         const cell = document.createElement("div");
-        cell.textContent = "bro";
+        cell.classList.toggle("cell");
+        cell.addEventListener("mouseenter", (e) => cell.classList.toggle("hovered"));
+        cell.addEventListener("mouseleave", (e) => cell.classList.toggle("hovered"));
         row.append(cell);
     }
     container.append(row);
