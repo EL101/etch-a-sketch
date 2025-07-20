29529624dfc7e571bb05b300ec6e2b71dfc4ff26
Ethan Liu <66912828+EL101@users.noreply.github.com>
2025-07-20 11:28:39 -0400

    Make sketch region a box centered on the screen. Start creating input slider

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ document.addEventListener("mousedown", () => isMouseDown = true);
 document.addEventListener("mouseup", () => isMouseDown = false);
 
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.toggle("row");
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement("div");
-        cell.classList.toggle("cell");
-        cell.addEventListener("mouseenter", (e) => {
-            if (isMouseDown) {
-                cell.classList.add("hovered");
-            }
-        });
-        cell.addEventListener("mousedown", () => cell.classList.add("hovered"));
-        // cell.addEventListener("mouseleave", (e) => cell.classList.toggle("hovered"));
-        row.append(cell);
+const CONTAINER_WIDTH = 500;
+function createCells(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.toggle("row");
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+            cell.classList.toggle("cell");
+            cell.addEventListener("mouseenter", (e) => {
+                if (isMouseDown) {
+                    cell.classList.add("hovered");
+                }
+            });
+            cell.addEventListener("mousedown", () => cell.classList.add("hovered"));
+            cell.style.width = CONTAINER_WIDTH / size + "px";
+            cell.style.height = cell.style.width;            
+            row.append(cell);
+        }
+        container.append(row);
     }
-    container.append(row);
 }
+
+createCells(16);
